babdd652135d5a969ca2811269b214df53feabb3
Paul Lessing <lessing.paul+github@gmail.com>
2021-01-13 21:27:19 +0000

    Add script to pack/unpack sub files

---
+++ pack.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+
+function printHelpAndExit() {
+  console.log(`
+Usage:
+ - pack.js <path/to/submarine.sub> [targetdir]
+   Unpacks a .sub file to XML.
+   If the targetdir parameter is passed, will unpack to that directory.
+   Otherwise, will unpack to the current working directory.
+ - pack.js <path/to/submarine.xml> [targetdir]
+   Packs an XML file to .sub format.
+   If targetdir is specified, the .sub file will be created in that folder.
+   Setting environment variable BAROTRAUMA_SUB_DIRECTORY will default to that directory.
+   Otherwise, will pack to the currnet working directory.\n`);
+  process.exit(1);
+}
+
+let file = process.argv[2];
+if (!file) {
+  printHelpAndExit();
+}
+
+const extname = path.extname(file);
+if (!~['.sub', '.xml'].indexOf(extname)) {
+  printHelpAndExit();
+}
+
+const isUnpack = path.extname(file) === '.sub';
+
+if (isUnpack) {
+  extractXml();
+} else {
+  createSub();
+}
+
+function createSub() {
+  const gzip = zlib.createGzip();
+  const fileIn = fs.createReadStream(file);
+
+  const targetDir = process.argv[3] || process.env.BAROTRAUMA_SUB_DIRECTORY || process.cwd();
+
+  const fileName = path.parse(file).name;
+  const outputFileName = path.join(targetDir, fileName + '.sub');
+  const fileOut = fs.createWriteStream(outputFileName);
+
+  fileIn.pipe(gzip).pipe(fileOut).on('finish', () => {
+    console.log(`Packed ${fileName}.xml to ${outputFileName}`);
+    process.exit(0);
+  });
+}
+
+function extractXml() {
+  const gunzip = zlib.createGunzip();
+
+  if (!fs.existsSync(file) &&
+    process.env.BAROTRAUMA_SUB_DIRECTORY && fs.existsSync(path.join(process.env.BAROTRAUMA_SUB_DIRECTORY, file))) {
+    file = path.join(process.env.BAROTRAUMA_SUB_DIRECTORY, file);
+  }
+  const fileIn = fs.createReadStream(file);
+
+  const fileName = path.parse(file).name;
+
+  const targetDir = process.argv[3] || process.cwd();
+  const outputFileName = path.join(targetDir, fileName + '.xml');
+
+  const fileOut = fs.createWriteStream(outputFileName);
+
+  fileIn.pipe(gunzip).pipe(fileOut).on('finish', () => {
+    console.log(`Extracted ${fileName}.sub to ${outputFileName}`);
+    process.exit(0);
+  });
+}
